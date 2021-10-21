145473e29b49390252a6e2776190d86b29da52ce
Sai-kishan-K <saik77556@gmail.com>
2021-10-21 16:50:10 +0530

    Prgression 2

--- hof/src/App.js
+++ hof/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import HigherOrderFunctions from './components/HigherOrderComponent';
+import HigherOrderComponent from './components/HigherOrderComponent';
 import './App.css';
 
 function App(){
   return (
     <div className="App">
       <HigherOrderComponent/>
+      {/* <HigherOrderComponent1/> */}
     </div>
   );
 }
--- hof/src/components/HigherOrderComponent.jsx
+++ hof/src/components/HigherOrderComponent.jsx
@@ -1,6 +1,7 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
 class HigherOrderComponent extends Component{
+    //PROGRESSION 1
     constructor(){
         super();
         this.state = {
@@ -12,5 +13,55 @@ class HigherOrderComponent extends Component{
                 { id: '5', name: 'Jack', user_type: 'Designer', age:43, years: 18}
             ]
         }
-    }        
+    }
+    //PROGRESSION 2
+    renderItems = () => {
+        const data = this.state.userData;
+        const mapRows = data.map((item) =>(
+            <React.Fragment key = {item.id}>
+                <li className = "list-elements">
+                    {/* Passing unique value to 'key' prop, eases process for virtual DOM to remove specific element and update HTML tree */}
+                    <span>Id: {item.id}</span>
+                    <span>Name: {item.name}</span>
+                    <span>User Type:{item.user_type}</span>
+                </li>
+            </React.Fragment>
+        ));
+        return mapRows;
+    };  
+    
+    // display all based on usertype
+
+    renderItems2 = (type) =>  {
+        const data = this.state.userData;
+        const mapRows = data.filter((item) => {
+            return item.user_type === type;
+        })
+        .map((item) => {
+            return <React.Fragment key ={item.id}>
+                <li className = "list-elements">
+                    <span>ID : {item.id}</span>
+                    <span>Name : {item.name}</span>
+                    <span>User Type : {item.user_type}</span>
+                </li>
+            </React.Fragment>
+        })
+        return mapRows;
+    }
+    render(){
+        return(
+            //insted of a parent div we can also use React.Fragment
+            <React.Fragment>
+                <h1>Display all items</h1>
+                <div className="display-box">
+                    <ul>{this.renderItems()}</ul>
+                </div>
+                <h1>Display based on type</h1>
+                <div className="display-box">
+                    <ul>{this.renderItems2('Designer')}</ul>
+                </div>
+            </React.Fragment>
+        )
+    };
 }
+export default HigherOrderComponent;
